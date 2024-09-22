0bbd8225cbd7da8b7a4227b8f53d7fad9a2c7106
zidehsaraee <moein.zideh@gmail.com>
2024-09-22 12:00:28 +0100

    set up domain for railway

--- backend/db/OnlineShop.js
+++ backend/db/OnlineShop.js
@@ -1,12 +1,8 @@
 require("dotenv").config();
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_PRIVATE_DOMAIN}:3306/${process.env.MYSQL_DATABASE}`;
+const connection = mysql.createConnection(urlDB);
 
 // Connect to MySQL
 connection.connect((err) => {
